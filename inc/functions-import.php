f846607bee94283ce37929e2b03a9c5a789a2022
smartcatdev <bilal@smartcat.ca>
2018-03-30 22:17:49 -0400

    improved preset-importing

--- inc/functions-import.php
+++ inc/functions-import.php
@@ -2,11 +2,14 @@
 
 namespace designr;
 
+add_filter( 'pt-ocdi/import_files', 'designr\import_options', 10 );
+
 function import_options() {
-    return array (
+    
+    return apply_filters( 'designr_presets', array (
         array (
             'import_file_name' => 'Demo Import 1',
-            'categories' => array ( 'Category 1', 'Category 2' ),
+            'categories' => array ( 'Category 1' ),
             'import_file_url' => 'http://www.your_domain.com/ocdi/demo-content.xml',
             'import_widget_file_url' => 'http://www.your_domain.com/ocdi/widgets.json',
             'import_customizer_file_url' => 'http://www.your_domain.com/ocdi/customizer.dat',
@@ -22,25 +25,15 @@ function import_options() {
         ),
         array (
             'import_file_name' => 'Demo Import 2',
-            'categories' => array ( 'New category', 'Old category' ),
+            'categories' => array ( 'Category 1' ),
             'import_file_url' => 'http://www.your_domain.com/ocdi/demo-content2.xml',
             'import_widget_file_url' => 'http://www.your_domain.com/ocdi/widgets2.json',
             'import_customizer_file_url' => 'http://www.your_domain.com/ocdi/customizer2.dat',
-            'import_redux' => array (
-                array (
-                    'file_url' => 'http://www.your_domain.com/ocdi/redux.json',
-                    'option_name' => 'redux_option_name',
-                ),
-                array (
-                    'file_url' => 'http://www.your_domain.com/ocdi/redux2.json',
-                    'option_name' => 'redux_option_name_2',
-                ),
-            ),
             'import_preview_image_url' => 'http://www.your_domain.com/ocdi/preview_import_image2.jpg',
             'import_notice' => __( 'A special note for this import.', 'your-textdomain' ),
             'preview_url' => 'http://www.your_domain.com/my-demo-2',
         ),
-    );
+    ));
+    
 }
 
-add_filter( 'pt-ocdi/import_files', 'designr\import_options' );
